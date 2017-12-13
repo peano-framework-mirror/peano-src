c264396bc1dae403fa761d09d435b1a5482890e5
tobiasweinzierl <tobiasweinzierl@aa977e5b-e928-4c84-aa66-c90e40304b12>
2017-12-13 10:26:11 +0000

    Homogenised the OpenMP variant with TBBs.
    git-svn-id: svn://svn.code.sf.net/p/peano/code/trunk/src@2982 aa977e5b-e928-4c84-aa66-c90e40304b12

--- tarch/multicore/omp/BackgroundTasks.cpp
+++ tarch/multicore/omp/BackgroundTasks.cpp
@@ -18,29 +18,65 @@ void tarch::multicore::spawnBackgroundTask(BackgroundTask* task) {
 }
 
 
+/**
+ * There are two different implementations: If there is only one background
+ * task or we are not allowed to use more than one thread to process background
+ * tasks anyway, then we simply run over _backgroundTasks and do all the jobs
+ * therein. Otherwise, we extract as many tasks as we are allowed to use
+ * threads and process those guys concurrently.
+ */
 bool tarch::multicore::processBackgroundTasks() {
-  int tasksToTrigger;
-  std::vector<tarch::multicore::BackgroundTask*>  triggeredTasks;
-
+  bool moreThanOneTask;
   #pragma omp critical(BackgroundCriticalSection)
   {
-    tasksToTrigger = std::min( _maxNumberOfBackgroundTasks, static_cast<int>(_backgroundTasks.size()) );
-    triggeredTasks = std::vector<tarch::multicore::BackgroundTask*>(_backgroundTasks.begin(),_backgroundTasks.begin()+tasksToTrigger);
-    _backgroundTasks.erase(_backgroundTasks.begin(),_backgroundTasks.begin()+tasksToTrigger);
+    moreThanOneTask = _maxNumberOfBackgroundTasks>1 && static_cast<int>(_backgroundTasks.size())>1;
   }
 
-  #pragma omp parallel for
-  for (int i=0; i<tasksToTrigger; i++) {
-    triggeredTasks[i]->run();
-    delete triggeredTasks[i];
+  if (moreThanOneTask) {
+    std::vector<tarch::multicore::BackgroundTask*>  triggeredTasks;
+    int tasksToTrigger;
+    #pragma omp critical(BackgroundCriticalSection)
+    {
+      tasksToTrigger = std::min( _maxNumberOfBackgroundTasks, static_cast<int>(_backgroundTasks.size()) );
+      triggeredTasks = std::vector<tarch::multicore::BackgroundTask*>(_backgroundTasks.begin(),_backgroundTasks.begin()+tasksToTrigger);
+      _backgroundTasks.erase(_backgroundTasks.begin(),_backgroundTasks.begin()+tasksToTrigger);
+    }
+
+    #pragma omp parallel for
+    for (int i=0; i<tasksToTrigger; i++) {
+      triggeredTasks[i]->run();
+      delete triggeredTasks[i];
+    }
   }
+  else {
+    #pragma omp critical(BackgroundCriticalSection)
+    {
+      for (int i=0; i<static_cast<int>(_backgroundTasks.size()); i++) {
+        triggeredTasks[i]->run();
+        delete triggeredTasks[i];
+      }
+      _backgroundTasks.clear();
+    }
+  }
+
 
   return tasksToTrigger;
 }
 
 
+/**
+ * To remain consistent with TBB versions, we do support -1 and 0. Please note
+ * that all background settings have different semantics anyway: We never run
+ * real background tasks but at least we can make the code decide how many
+ * cores it uses in doubt to process the background tasks available.
+ */
 void tarch::multicore::setMaxNumberOfRunningBackgroundThreads(int maxNumberOfRunningBackgroundThreads) {
-  assertion(maxNumberOfRunningBackgroundThreads>0);
+  assertion(maxNumberOfRunningBackgroundThreads>=-1);
+  if (maxNumberOfRunningBackgroundThreads<=0) {
+    static tarch::logging::Log _log( "tarch::multicore" );
+    logWarning( "setMaxNumberOfRunningBackgroundThreads", "OpenMP realisation does not support natively -1 and 0 as number of background tasks. Fall back to 1" );
+    maxNumberOfRunningBackgroundThreads = 1;
+  }
   _maxNumberOfBackgroundTasks = maxNumberOfRunningBackgroundThreads;
 }
 
